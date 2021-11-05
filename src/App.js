5bdb3d75e6f3f543bdd801c1cde4a9cd85f9e162
Joes-BitGit <josephnalmeida@gmail.com>
2021-11-04 20:48:10 -0700

    added some layout

--- src/App.js
+++ src/App.js
@@ -3,7 +3,24 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">'ello Gov'na</header>
+      <header className="App-header">
+        <h1>(JA)</h1>
+        <nav className="navbar">
+          <ul>
+            <li>Home</li>
+            <li>Projects</li>
+          </ul>
+        </nav>
+      </header>
+      <main className="main-content">
+        <div className="intro-blurb">Hello I'm Joe</div>
+        <div className="about-me">
+          <img src="" alt="profile shot of joseph" />
+        </div>
+        <div className="highlighted-work">ram to lineup 1-line layout</div>
+        <div className="closing-remarks">Say Hello to Me ;P</div>
+      </main>
+      <footer className="end-content">Designed by your boy</footer>
     </div>
   );
 }
