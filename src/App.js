6a7b996a6516c62bfb8dcc7a2aa067ba4455e38a
Joes-BitGit <josephnalmeida@gmail.com>
2021-12-02 20:25:13 -0800

    added more content + classnames

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import "./index.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>{"{ JA }"}</h1>
+        <p className="logo-initials">{"{ JA }"}</p>
         <nav className="navbar">
           <ul>
             <li>Home</li>
@@ -43,7 +44,7 @@ function App() {
           </a>
         </section>
         <section className="about-me">
-          <img src="" alt="profile shot of Joseph" />
+          <h3 className="about-section">About Me</h3>
           <p>
             Yo! My name is Joseph Almeida, I've been close to a computer since
             an early age but did not start programming until I reached
@@ -53,8 +54,10 @@ function App() {
             When I'm not coding I play games with my friends, work out, or when
             the weather permits some amateur astronomy
           </p>
+          <img src="" alt="profile shot of Joseph" />
         </section>
         <section className="highlighted-work">
+          <h3 className="projects">Highlighted Work</h3>
           <div className="parent">
             <div className="card 1">
               <h4>Pomodoro</h4>
@@ -86,7 +89,23 @@ function App() {
             </div>
           </div>
         </section>
-        <section className="closing-remarks">Say Hello to Me ;P</section>
+        <section className="closing-remarks">
+          <h3 className="contact-info">Stay In Touch</h3>
+          <p>Say Hello to Me ;P</p>
+          <a
+            href="http://linkedin.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            L
+          </a>
+          <a href="http://github.com" target="_blank" rel="noopener noreferrer">
+            G
+          </a>
+          <a href="http://google.com" target="_blank" rel="noopener noreferrer">
+            Email
+          </a>
+        </section>
       </main>
       <footer className="end-content">Designed by your boy</footer>
     </div>
